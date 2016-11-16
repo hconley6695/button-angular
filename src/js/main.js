11b427c0884821b42c0b3872f02a55d433c84aac
Heather Conley <hconley6695@gmail.com>
2016-11-16 17:07:11 -0500

    final simple working button in angular

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,10 @@ function MainController($scope) {
 	console.log('hello');
 	$scope.countUp = 0;
 
+
 	$scope.counter = function () {
-		$scope.countUp = $scope.countUp + 1;
+			$scope.countUp = $scope.countUp + 1;
+			console.log('Waz up??');
 	}
 }
 
